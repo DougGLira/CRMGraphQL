45026d0a5fadd297266a534790b375eb192b7a1b
DougGLira <dgl.mgc@gmail.com>
2020-10-03 21:35:50 -0600

    Update apollo.js

--- crmcliente/config/apollo.js
+++ crmcliente/config/apollo.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:4000/',
+    uri: 'https://secure-falls-59821.herokuapp.com/',
     fetch
 });
 
